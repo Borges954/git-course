fe77399fa66d65ef471c2970c9476fd17582e4a8
Jose Isaak <joseisaakborgespuc@gmail.com>
2019-12-04 14:11:47 -0600

    Contenido agregado

--- index.php
+++ index.php
@@ -38,6 +38,100 @@
 </nav>
 
 
+<form role="form">
+  <div class="form-group">
+    <label class="control-label" for="email-01">Correo electrónico:</label>
+    <input class="form-control" id="email-01" placeholder="Correo electrónico" type="text">
+  </div>
+  <div class="form-group">
+    <label class="control-label" for="password-01">Contraseña</label>
+    <input class="form-control" id="password-01" placeholder="Contraseña" type="password">
+  </div>
+  <div class="form-group">
+    <label class="control-label" for="file-01">Cargar archivo:</label>
+    <input id="file-01" type="file">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox">
+      Acepto los términos
+    </label>
+  </div>
+  <div class="form-group">
+    <textarea class="form-control" rows="3"></textarea>
+  </div>
+  <button class="btn btn-primary pull-right" type="submit">Enviar</button>
+</form>
+
+
+<form class="form-inline" role="form">
+  <div class="form-group">
+    <label class="sr-only" for="email-02">Correo electrónico:</label>
+    <input class="form-control" id="email-02" placeholder="Ingrese su email" type="text">
+  </div>
+  <div class="form-group">
+    <label class="sr-only" for="password-02">Contraseña</label>
+    <input class="form-control" id="password-02" placeholder="Ingrese su contraseña" type="password">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox">
+      Recordarme
+    </label>
+  </div>
+  <button class="btn btn-primary" type="submit">Ingresar</button>
+</form>
+
+
+<form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label class="col-sm-3 control-label" for="email-03">Correo electrónico:</label>
+    <div class="col-sm-9">
+      <input class="form-control" id="email-03" placeholder="Correo electrónico" type="text">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-sm-3 control-label" for="password-03">Contraseña:</label>
+    <div class="col-sm-9">
+      <input class="form-control" id="password-03" placeholder="Contraseña" type="password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-3 col-sm-9">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox">
+          Recordarme
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-3 col-sm-9">
+      <button class="btn btn-primary pull-right" type="submit">Enviar</button>
+    </div>
+  </div>
+</form>
+
+
+
+<select class="form-control">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+...
+<select multiple class="form-control">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+
+
 	<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
 </body>
 </html>
